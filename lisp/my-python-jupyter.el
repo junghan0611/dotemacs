8a823ceaae67fa3f9239b7d1f9862dddb7137112
junghan0611 <31724164+junghan0611@users.noreply.github.com>
2025-02-23 12:51:55 +0900

    🐛 update my-python-jupyter for jupyter repl interaction

--- lisp/my-python-jupyter.el
+++ lisp/my-python-jupyter.el
@@ -1,10 +1,12 @@
 ;;; lisp/my-python-jupyter.el -*- lexical-binding: t; -*-
 
+;; https://github.com/emacs-jupyter/jupyter/pull/573/files
+
+;;;; jupyter code completion with corfu
+
 ;; dotsamples/vanilla/karthink-dotfiles-popper/lisp/setup-python.el
-
-;;;; jupyter - corfu and company
-
 (with-eval-after-load 'jupyter
+
   ;; :bind (:map jupyter-repl-interaction-mode-map
   ;;             ("M-i"   . nil)
   ;;             ("C-h ." . jupyter-inspect-at-point))
@@ -78,6 +80,88 @@ Use the `company-doc-buffer' to insert the results."
   ;;         ;; (:tangle . "jupyter-python/tangled.py")
   ;;         ;; (:exports . "both")
   ;;         ))
+  )
+
+;;;; jupyter - repl interaction
+
+;; starterkit/snakemacs-python/main.el
+;; sqrt-dotfiles-elfeed/Emacs.org
+
+(progn
+
+  (setq jupyter-repl-echo-eval-p t) ; default nil
+
+  ;; When this minor mode is enabled you may evaluate code from the current
+  ;; buffer using the associated REPL (see jupyter-repl-associate-buffer to
+  ;; associate a REPL).
+
+  (defun my/jupyter-refresh-kernelspecs ()
+    "Refresh Jupyter kernelspecs"
+    (interactive)
+    (jupyter-available-kernelspecs t))
+
+  (defun my/jupyter-refesh-langs ()
+    "Refresh Jupyter languages"
+    (interactive)
+    (org-babel-jupyter-aliases-from-kernelspecs t))
+
+  (setq my/jupyter-runtime-folder
+        (expand-file-name "~/.local/share/jupyter/runtime"))
+
+  (defun my/get-open-ports ()
+    (mapcar
+     #'string-to-number
+     (split-string (shell-command-to-string
+                    "ss -tulpnH | awk '{print $5}' | sed -e 's/.*://'")
+                   "\n")))
+
+  (defun my/list-jupyter-kernel-files ()
+    (mapcar
+     (lambda (file)
+       (cons
+        (car file)
+        (cdr (assq 'shell_port (json-read-file (car file))))))
+     (sort
+      (directory-files-and-attributes my/jupyter-runtime-folder
+                                      t ".*kernel.*json$")
+      (lambda (x y) (not (time-less-p (nth 6 x) (nth 6 y)))))))
+
+  (defun my/select-jupyter-kernel ()
+    (let ((ports (my/get-open-ports))
+          (files (my/list-jupyter-kernel-files)))
+      (completing-read
+       "Jupyter kernels: "
+       (seq-filter (lambda (file) (member (cdr file) ports)) files))))
+
+  ;; #+PROPERTY: header-args:python :session <path-to-kernel>
+  (defun my/insert-jupyter-kernel ()
+    "Insert a path to an active Jupyter kernel into the buffer"
+    (interactive)
+    (insert (my/select-jupyter-kernel)))
+
+  (defun my/jupyter-connect-repl ()
+    "Open an emacs-jupyter REPL, connected to a Jupyter kernel"
+    (interactive)
+    (jupyter-connect-repl (my/select-jupyter-kernel) nil nil nil t))
+
+  (defun my/jupyter-qtconsole ()
+    "Open Jupyter QtConsole, connected to a Jupyter kernel"
+    (interactive)
+    (start-process "jupyter-qtconsole" nil "setsid" "jupyter" "qtconsole" "--existing"
+                   (file-name-nondirectory (my/select-jupyter-kernel))))
+
+  (defun my/jupyter-cleanup-kernels ()
+    (interactive)
+    (let* ((ports (my/get-open-ports))
+           (files (my/list-jupyter-kernel-files))
+           (to-delete
+            (seq-filter
+             (lambda (file) (not (member (cdr file) ports))) files)))
+      (when (and (length> to-delete 0)
+                 (y-or-n-p
+                  (format "Delete %d files?" (length to-delete))))
+        (dolist (file to-delete)
+          (delete-file (car file))))))
   )
 
 ;;; provide
